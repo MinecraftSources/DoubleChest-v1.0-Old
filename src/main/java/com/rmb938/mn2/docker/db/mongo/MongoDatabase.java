bb74df9e6eacb98ab595c082a82c66c25572cd52
Jodie <rmb1993@gmail.com>
2014-07-10 10:25:25 -0400

    added start of mongo

---
+++ src/main/java/com/rmb938/mn2/docker/db/mongo/MongoDatabase.java
@@ -0,0 +1,10 @@
+package com.rmb938.mn2.docker.db.mongo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MongoDatabase {
+
+    private static final Logger logger = LogManager.getLogger(MongoDatabase.class.getName());
+
+}
