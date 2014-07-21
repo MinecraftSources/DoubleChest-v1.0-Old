039d1123cfc4589e3834ae2d7027b75d474c192b
Jodie <rmb1993@gmail.com>
2014-07-21 15:48:56 -0400

    remove aerospike

--- src/main/java/com/rmb938/mn2/docker/db/aerospike/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.rmb938.mn2.docker.db.aerospike;
-
-
-import java.util.List;
-
-public interface Handler<T> {
-
-    public abstract T handle(AerospikeDatabase database, String query) throws ParserException;
-}
--- src/main/java/com/rmb938/mn2/docker/db/aerospike/ParserException.java
+++
@@ -1,9 +0,0 @@
-package com.rmb938.mn2.docker.db.aerospike;
-
-public class ParserException extends Exception {
-
-    public ParserException(String message) {
-        super(message);
-    }
-
-}
